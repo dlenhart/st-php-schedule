d68d8b8229117b220c7173fa1d944e4b8a7140e2
Drew D. Lenhart <dlenhart@gmail.com>
2016-07-26 10:30:51 -0400

    updates/screenshot

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -13,29 +13,24 @@ use stphpschedule\Model\Queue;
  */
 class HomepageController extends AbstractController
 {
-    public function home(Request $request, Response $response, $args)
-    {
+	public function home(Request $request, Response $response, $args)
+	{
 		$body = $this->view->fetch('home_interface.html');
-
-        return $response->write($body);
-    }
-	
+		return $response->write($body);
+	}
 	public function queue(Request $request, Response $response, $args)
-    {
+	{
 		$que = New Queue;
 		$que = Queue::all();
 		$body = $this->view->fetch('home_queue.html', array('qItems' => $que));
-
-        return $response->write($body);
-    }
-	
+		return $response->write($body);
+	}
 	public function job(Request $request, Response $response, $args)
-    {
+	{
 		$job = New Jobs;
 		$job = Jobs::all();
 		$body = $this->view->fetch('home_job.html', array('jItems' => $job));
-
-        return $response->write($body);
-    }
+		return $response->write($body);
+	}
 }
 
